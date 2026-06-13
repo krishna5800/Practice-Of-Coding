3c57b9aedc72591f3f6278c99d332e0a47a9690f
Krishna Hitnalikar <krishnahitnalikar4@gmail.com>
2026-06-13 23:57:43 +0530

    code

---
+++ Java_Codes/Test.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+class Test
+{
+    public static void main(String Arr[])
+    {
+        Scanner sobj = new Scanner(System.in);
+
+        int iNo = 0;
+        int iCount = 0;
+
+        System.out.println("Enter Number : ");
+        iNo = sobj.nextInt();
+
+        for(int i = 0; i < 3; i++)
+        {
+            System.out.print("* ");
+        }
+
+    }
+}
